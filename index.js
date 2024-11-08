1ad6cc8c31a74aed42ce5515ec8673360955a513
AliAgayevv <aghayev_233@mail.ru>
2024-11-09 02:57:32 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const ReactX = (() => {
+    let state = [];
+
+    let index = 0;
+    const useState = (initialValue) => {
+        const localIndex = index;
+        index++;
+
+        if(state[localIndex] === undefined)
+            state[localIndex] = initialValue
+
+        const setterFunction = (newValue) => {
+            state[localIndex] = newValue;
+        }
+
+        return [state[localIndex], setterFunction];
+    };
+
+    const resetIndex = () =>
+    {
+        index = 0;
+    }
+
+    return {
+        useState, resetIndex,
+    };
+})();
+
+const { useState, resetIndex } = ReactX;
+
+const Component = () => {
+    const [counterValue, setCounterValue] = useState(1);
+
+    console.log(counterValue)
+
+    if (counterValue !== 2) {
+        setCounterValue(2);
+
+    }
+};
+
+Component();
+resetIndex();
+Component();
+
