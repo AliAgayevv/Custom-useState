59f1b62567528bf7f84a1ea38dc727c047e69154
AliAgayevv <aghayev_233@mail.ru>
2024-11-09 02:59:27 +0400

    done

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const ReactX = (() => {
+    	// eger state i useState ichinde init elesek, her defe cagrilanda 1 den baslayacaq bunun qarshisin almaq ucun evvelnce state adli variable yaradiriq sonra ise deyer veririk
     let state = [];
 
     let index = 0;
     const useState = (initialValue) => {
+          // state undefined yeni hele icine deyer qoyulmayibsa initialValue argumentini icine atiriq
         const localIndex = index;
         index++;
 
         if(state[localIndex] === undefined)
             state[localIndex] = initialValue
-
+            // state i artiracaq funksiya    
         const setterFunction = (newValue) => {
             state[localIndex] = newValue;
         }
@@ -26,13 +28,16 @@ const ReactX = (() => {
     };
 })();
 
+
+
 const { useState, resetIndex } = ReactX;
 
 const Component = () => {
+    	// ReactX icinde fake state yaratmisdiq, onu basladiriq.
     const [counterValue, setCounterValue] = useState(1);
 
     console.log(counterValue)
-
+// deyer 2 deilse 2 ye teyin edirik, infinite loop qarshisin almaq uchundur
     if (counterValue !== 2) {
         setCounterValue(2);
 
